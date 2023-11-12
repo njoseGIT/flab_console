e5622f1c18ebdd0f4d61f536ad8add2ecb732f66
njoseGIT <njose40707@gmail.com>
2023-11-12 20:18:51 +0000

    Initial commit

---
+++ Tasks/ConsoleFlabProcess.py
@@ -0,0 +1,48 @@
+#A class for running the main flab process
+import sys
+
+class Task():
+    task_name = 'ConsoleFlabProcess'
+    task_type = 'process'
+
+    def __init__(self, flab):
+        self.flab = flab
+
+    #method called when process is running
+    def run(self, flab, in_queue, out_queue):
+        try:
+            self.flab = flab
+            self.in_queue = in_queue
+            self.out_queue = out_queue
+            while self.flab.is_running:
+                command = self.in_queue.get(block=True)
+                try:
+                    if command == 'close':
+                        self.flab.is_running = False
+                    elif command != '':
+                        if command[0:6] == 'start ':
+                            task_info = command[6:]
+                            eval('self.flab.start_task("' + task_info +'")')
+                        elif command[0:7] == 'rstart ':
+                            task_info = command[7:]
+                            eval('self.flab.reload_start_task("' + task_info +'")')
+                        elif command[0:5] == 'stop ':
+                            task_info = command[5:]
+                            eval('self.flab.stop_task("' + task_info +'")')
+                        else:
+                            eval('self.flab.' + command)
+                    else:
+                        pass
+                except Exception as e:
+                    self.out_queue.put(str(e))
+                    self.out_queue.put('error in flab command')
+
+        except Exception as e:
+            self.flab.display('Error in ConsoleFlabProcess')
+            self.flab.display(e)
+
+
+
+
+
+
---
+++ Tasks/ConsoleUIProcess.py
@@ -0,0 +1,35 @@
+from UIs import ConsoleUI
+from PyQt5 import QtWidgets
+import sys
+
+class Task():
+    task_name = 'ConsoleUIProcess'
+    task_type = 'process'
+    queues = ()
+    proxies = ()
+    pipes = ()
+
+    def __init__(self, flab):
+        self.flab = flab
+
+    #method called when process is running
+    def run(self, flab, ui_queue, flab_queue, project_path = ''):
+
+        try:
+            self.flab = flab
+            self.ui_queue = ui_queue
+            self.flab_queue = flab_queue
+            app = QtWidgets.QApplication(sys.argv)
+            #print('project_path= ' + str(project_path))
+            ui = Console3UI.GUI(flab, self.ui_queue, self.flab_queue, project_path = project_path)
+            ui.run()
+            ui.list_loaded_tasks()
+            app.exec_()
+
+        except Exception as e:
+            self.flab.display('Error in ConsoleUI')
+            self.flab.display(e)
+
+
+
+
